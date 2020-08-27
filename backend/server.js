d9907df565bc0292f78740d35ac227fde6da9d8c
karthicksndr <karthicksndr@gmail.com>
2020-08-27 23:40:03 +1200

    push

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const userRouter= require('./routes/user');
 require('dotenv').config();
 
 const app= express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +16,7 @@ const uri= process.env.ATLAS_URI;
 mongoose.connect(uri,{
     useNewUrlParser: true,
      useCreateIndex:true,
+     useUnifiedTopology: true
     });
 const connection= mongoose.connection;
 connection.once('open',() =>{
--- src/components/createUser.component.js
+++ src/components/createUser.component.js
@@ -1,10 +1,55 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class CreateUser  extends Component{
+    constructor(props) {
+        super(props);
+    
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+          username: ''
+        }
+      }
+
+      onChangeUsername(e) {
+        this.setState({
+          username: e.target.value
+        })
+      }
+
+      onSubmit(e) {
+        e.preventDefault();
+        const user = {
+          userName: this.state.username,
+        }
+        console.log(user)
+        axios.post('http://localhost:5000/user/add', user)
+        .then(res => console.log(res.data))
+        this.setState({
+            username: ''
+        })
+    }
+
     render() {
         return (
-            <div> 
-                <p> Create User Component</p>
+            <div>
+                <h3> Create User</h3>
+                <form onSubmit= {this.onSubmit}>
+                <div className="form-group">
+                    <label> User Name: </label>
+                    <input type="text" 
+                    required
+                    className= "form-control"
+                    value= {this.state.userName}
+                    onChange= {this.onChangeUsername}
+                    />
+                </div>
+                <div className="form-gourp">
+                    <input type="submit" value="Create User" className="btn btn-primary" />
+                </div>                
+                </form>      
             </div>
         );
     }
